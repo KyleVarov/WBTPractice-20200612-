1bb2332580330a4740473e71f97b0646c8294fc7
kylevarov <kyrill.spiegl@gmail.com>
2020-06-12 11:36:09 +0900

    main method layout fix

--- Main/MyApp.java
+++ Main/MyApp.java
@@ -13,7 +13,7 @@ public class MyApp
         System.out.println("자기 나이를 입력하세요:");
         Scanner scan = new Scanner(System.in);
         int age = scan.nextInt();
-        System.out.println("DVD는 구작품인가요? 구작품이면 'true'입력하고 아니면 'false'입력하세요:");
+        System.out.println("DVD는 구작품인가요?\n구작품이면 'true'입력하고 아니면 'false'입력하세요:");
         boolean oldMovie = scan.nextBoolean();
         DVDSaleCalculate dSC = new DVDSaleCalculate();
         int price = dSC.DVDSaleCalculate(oldMovie,age);
