f00e749aab7bdb8548656957acf6297bcef3b1ef
kylevarov <kyrill.spiegl@gmail.com>
2020-06-12 10:54:27 +0900

    project initialized

--- Calculation/DVDSaleCalculate.java
+++ Calculation/DVDSaleCalculate.java
@@ -1,20 +1,19 @@
 package Calculation;
-
 /**
- * 렌탈하는 DVD�?� 할�?�여부�? 따�?� 요금�?� 계산해주는 메소드�?�다.
+ * 렌탈하는 DVD의 할인여부에 따라 요금을 계산해주는 메소드이다.
  * 
- * @author (박채린 , �?�와사키메구미 , 마츠바�?�케�?�토) 
+ * @author (박채린 , 이와사키메구미 , 마츠바라케이토) 
  * @version (2020.06.11)
  */
 public class DVDSaleCalculate
 {
     /**
      * @param
-     * Date = 구작(대여시작�?�로부터 6개월�?��? 경과�?� 작품) �?�지 아닌지 �?단한 값
-     * (True = 구작�?� 경우 / False = 구작�?� 아�?경우)
-     * age = 나�?�값
+     * Date = 구작(대여시작일로부터 6개월이상 경과된 작품) 인지 아닌지 판단한 값
+     * (True = 구작일 경우 / False = 구작이 아닐경우)
+     * age = 나이값
      * charge = 요금
-     * @return = 할�?��?� �?용�?� 후�?� 요금
+     * @return = 할인이 적용된 후의 요금
      */
     boolean Date = true;
     int age = 0;
