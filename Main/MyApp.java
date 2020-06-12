c63335d9aa58fc7510124fd6360689065cb2f8b9
kylevarov <kyrill.spiegl@gmail.com>
2020-06-12 11:30:33 +0900

    main method fix

--- Main/MyApp.java
+++ Main/MyApp.java
@@ -10,9 +10,14 @@ import Calculation.*;
 public class MyApp
 {
     public static void main(String[] args){
+        System.out.println("자기 나이를 입력하세요:");
         Scanner scan = new Scanner(System.in);
         int age = scan.nextInt();
+        System.out.println("DVD는 구작품인가요? 구작품이면 'true'입력하고 아니면 'false'입력하세요:");
         boolean oldMovie = scan.nextBoolean();
-        int price = DVDSaleCalculate(oldMovie,age);
+        DVDSaleCalculate dSC = new DVDSaleCalculate();
+        int price = dSC.DVDSaleCalculate(oldMovie,age);
+
+        System.out.println("할인가격: " + price);
     }
 }
